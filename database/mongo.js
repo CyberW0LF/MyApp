c2d73570215a04621a2d5b2b7afffdfbb7a0298c
CyberW0LF <firsovkukiit@gmail.com>
2018-12-28 13:54:48 +0300

    add try catch

--- database/mongo.js
+++ database/mongo.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 module.exports = {
   connect: async () => {
-    const db = await mongoose.connect(
-      process.env.MONGO_URL,
-      { useNewUrlParser: true }
-    );
+    try {
+      const db = await mongoose.connect(
+        process.env.MONGO_URL,
+        { useNewUrlParser: true }
+      );
+    } catch (error) {
+      new Error(error);
+    }
   }
 };
